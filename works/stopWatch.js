1d5a7aabaceb128545bea266d3fcf2c986bfb4ec
ryoha_0216 <the7uya@gmail.com>
2019-05-10 12:39:22 +0900

    ストップウォッチの開始を追加(停止はいつできるんだ…)

--- works/stopWatch.js
+++ works/stopWatch.js
@@ -5,6 +5,9 @@ const start = () => {
   countElement.innerText = '秒数: ' + time + '秒'
 }
 
+ver id = setInterval('start()',1000)
+
+
 const reset = () => {
     time = 0
     const countElement = document.querySelector('#count')
